674fe7d6799fb860c29970f028016ec61fb70ab5
Wachira Duangdee <wcrcorp@gmail.com>
2022-10-24 16:47:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ urllib3.disable_warnings()
 
 # Example with context manager
 
-# with Unifi_API(username="wachiradd", password="Mst001Admin", baseurl="https://192.168.4.170:8443", verify_ssl=False) as api:
+# with Unifi_API(username="user1", password="pass1", baseurl="https://192.168.4.170:8443", verify_ssl=False) as api:
 #     device_list = (api.list_clients(filters={'hostname': 'Chromecast.*'}, order_by="ip"))
 #     print(json.dumps(device_list, indent=4))
 
