ee849a76ea511329d79d47b98285856fc1db9302
Wachira Duangdee <wcrcorp@gmail.com>
2022-10-24 16:46:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ urllib3.disable_warnings()
 
 # Example without contextmanager
 
-api = Unifi_API(username="wachiradd", password="Mst001Admin", baseurl="https://192.168.4.170:8443", verify_ssl=False)
+api = Unifi_API(username="user1", password="pass1", baseurl="https://192.168.4.170:8443", verify_ssl=False)
 api.login()
 device_list = (api.list_clients(order_by="ip"))
 print(json.dumps(device_list, indent=4))
